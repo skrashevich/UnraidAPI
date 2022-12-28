25dc276afb406a65884785d537050746386c78e7
Sergey Krashevich <svk@svk.su>
2022-12-28 09:19:34 +0300

    UnRAID 6.11 support

--- api/getServers.js
+++ api/getServers.js
@@ -1,5 +1,5 @@
 import fs from "fs";
-import { getUnraidDetails } from "../utils/Unraid";
+import { getUnraidDetails, startCommand } from "../utils/Unraid";
 
 export default function(req, res, next) {
   let servers = {};
