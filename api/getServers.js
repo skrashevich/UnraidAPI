559b2972fe8087541309815e6653b42c3342776b
Sergey Krashevich <svk@svk.su>
2023-01-26 16:12:05 +0300

    Revert "Merge branch 'unraid611' of https://github.com/skrashevich/UnraidAPI into unraid611"
    This reverts commit 2868365a0274a447c29396fd7838cdf45fafa7c8, reversing
    changes made to b7f646671eb1b3ef30d0769f2c11c383ec24bef9.

--- api/getServers.js
+++ api/getServers.js
@@ -1,5 +1,5 @@
 import fs from "fs";
-import { getUnraidDetails, startCommand } from "../utils/Unraid";
+import { getUnraidDetails } from "../utils/Unraid";
 
 export default function(req, res, next) {
   let servers = {};
